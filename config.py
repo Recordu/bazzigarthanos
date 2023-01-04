fa9dff9abcee5874a1fc37d0ab141da2d44e9025
Recordu <117724279+Recordu@users.noreply.github.com>
2023-01-04 08:16:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,19 +6,19 @@ class Development(Config):
     APP_ID = 10248430
     API_HASH = "42396a6ff14a569b9d59931643897d0d"
     # the name to display in your alive message
-    ALIVE_NAME = "rishabh"
+    ALIVE_NAME = "baazigar"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "postgres://jrycfssc:fEZMYWiOcO9vB8cIq1_Z97GhW3TiTppJ@tiny.db.elephantsql.com/jrycfssc"
+    DB_URI = "postgres://zxzoeuii:jbCEk7pc_92nGvb_NBcBpA1TvxtlOsEm@satao.db.elephantsql.com/zxzoeuii"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "your session"
+    STRING_SESSION = "1BVtsOHwBu1k-EK3jvZI0F-C3dOx6h5v0r8k-9rD87muLNRkvv8b38CzhLTzAXp4-1qFul6Fn9nT7lQzrve3IjfIrM9nVLeReGrufZhibTwq1a-ypxuhmZdKsG0lRNdc1HG4evvxIfjTtS5xPzp6mxj-zINfdNWM0ivpEjf0HbXZrCyHaqGwlooVj3ClglOmt7xSb7WEtGMK-bncodsXRz4cdWmsrjacF0sq3As8ttlb3UGbMMx8GEonSFVs_EPAeA-Kmbg2ymRvKJHL_hnHfuFAa3JX6XPlJNhOag7E-SVdHJ5NEsXx6NciVUBeaku_dRkhDNr12BL75niRtzOzNE-NS68E4GRE="
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "your token"
+    TG_BOT_TOKEN = "5909739573:AAHenw5lZ9Diy6IeHOoJhe9GU_HNKExXMiU"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID =
+    PRIVATE_GROUP_BOT_API_ID =-1001752915769
     # command handler
     COMMAND_HAND_LER = "."
     VCMODE = "True"
-    VC_SESSION = "your 2nd id telethon session" #note don't use maim account to vc player
+    VC_SESSION = "1BVtsOHwBu1k-EK3jvZI0F-C3dOx6h5v0r8k-9rD87muLNRkvv8b38CzhLTzAXp4-1qFul6Fn9nT7lQzrve3IjfIrM9nVLeReGrufZhibTwq1a-ypxuhmZdKsG0lRNdc1HG4evvxIfjTtS5xPzp6mxj-zINfdNWM0ivpEjf0HbXZrCyHaqGwlooVj3ClglOmt7xSb7WEtGMK-bncodsXRz4cdWmsrjacF0sq3As8ttlb3UGbMMx8GEonSFVs_EPAeA-Kmbg2ymRvKJHL_hnHfuFAa3JX6XPlJNhOag7E-SVdHJ5NEsXx6NciVUBeaku_dRkhDNr12BL75niRtzOzNE-NS68E4GRE=" #note don't use maim account to vc player
     # command hanler for sudo
     SUDO_COMMAND_HAND_LER = "."
     # External plugins repo
